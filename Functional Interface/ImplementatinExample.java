e69fb0399d41f0ff9cdb0ab354ac53ea82df638a
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-09-04 16:19:53 +0530

    Update ImplementatinExample.java

--- Functional Interface/ImplementatinExample.java
+++ Functional Interface/ImplementatinExample.java
@@ -4,8 +4,9 @@ interface Interf{
 
 public class Test{
 	public static void main(String [] args){
-		Interf i = ()-> System.out.println("Hello...");
-    i.m1();
+		Interf i = ()-> System.out.println("Hello..."); /* we can provide implementation to abstract method by lambda expression, and since we
+		can have only one method then this implemenation is surely for that method only */
+    		i.m1();
 	}
 }
 
