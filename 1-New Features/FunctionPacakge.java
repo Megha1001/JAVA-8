be77b39d1a8d4576cfcdf063259701ed84d714b6
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-09-04 11:50:37 +0530

    Create FunctionPacakge.java

---
+++ 1-New Features/FunctionPacakge.java
@@ -0,0 +1,16 @@
+import java.util.function.*;
+
+public class Square{
+  public static void main(String [] args){
+    Function<Integer, Integer> f = i -> i * i; // Function<Input,Output> f = input -> output;
+    
+    System.out.println("Square of 40 : "+f.apply(40));
+    System.out.println("Square of 50 : "+f.apply(50));
+  }
+}
+
+/*
+OUTPUT : 
+Square of 401600
+Square of 502500
+*/
