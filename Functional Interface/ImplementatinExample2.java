7f4a2282770c0d9a7f86185d28a20f566396021a
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2022-09-04 16:28:26 +0530

    Update ImplementatinExample2.java

--- Functional Interface/ImplementatinExample2.java
+++ Functional Interface/ImplementatinExample2.java
@@ -6,7 +6,7 @@ interface Interf{
 
 public class Test{
 	public static void main(String [] args){
-		Interf i = (a,b)->System.out.println("Sum is "+ (a+b));
+		Interf i = (a,b)->System.out.println("Sum is "+ (a+b));//here , compiler always consider abstract method of Interf, so compiler can able to guess the type
 		i.add(10,20);
 	}
 }
